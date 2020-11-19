c072d91a64c4a8cb617b8c168a691b071b22ae12
Saurabh Pandey <spandey211@gmail.com>
2020-11-19 18:33:07 +0530

    Second Commit

--- Example/SPDateFormattor/AppDelegate.swift
+++ Example/SPDateFormattor/AppDelegate.swift
@@ -15,8 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+         print("First Commit Test")
         // Override point for customization after application launch.
-        print("First Commit Test")
+       
+        print("Second Commit Test")
+
         return true
     }
 
